04eda240482727356bd8837bc5b48c1a79084351
quinlander <rryanq@gmail.com>
2019-05-07 15:14:38 -0400

    Modified description updater to easily operate on all videos on a channel
    - made the video_ids option optional and set default to operate on all video_ids of a given channel. This makes it easy to do a quick find and replace without having to generate a current list of video_ids on a given channel
    - added one test input
    - minor bug fixes
    - program now dependent on a file called 'api_credentials.json' which has not been pushed
    - docs updated to describe how to write an api credentials file in order to run the program with the default video_ids setting

--- v2/update_description_v2.py
+++ v2/update_description_v2.py
@@ -17,6 +17,9 @@ from oauth2client.tools import argparser, run_flow
 
 # specifies location of client secrets JSON object
 CLIENT_SECRETS_FILE = "client_secrets_v2.json"
+# This is a way to access an API key and hardcoded channel ID for the purpose
+# of obtaining a  list of all video ids for a given channel
+API_CREDENTIALS_FILE = "api_credentials.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account.
@@ -42,22 +45,39 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
 
+# define Python user-defined exceptions
+class Error(Exception):
+   """Base class for other exceptions."""
+   pass
+
+class StringNotFoundError(Error):
+   """Raised when the string is not found."""
+   pass
 
 @click.command()
-@click.option('--video_ids', required=True, type=str, help="File containing the list of video IDs of all YouTube videos to operate on.")
+@click.option('--video_ids', required=False, type=str, help="File containing the list of video IDs of all YouTube videos to operate on.")
 @click.option('--find', required=True, type=str, help="File containing the string that you would like to find in your YouTube descriptions.")
 @click.option('--replace_with', required=True, type=str, help="File containing the string that you would like to replace the find string with.")
 def main(video_ids, find, replace_with):
 
-    # extract list of video IDs from provided file
-    with open(video_ids) as file:
-        video_ids = json.load(file)
-
     # Initialize argparser arguments
     argparser.add_argument("--video_ids", default=video_ids)
-    argparser.add_argument("--video-id", help="ID of video to update.", default="")
+    argparser.add_argument("--video_id", help="ID of video to update.", default="")
     argparser.add_argument("--find", default=find)
     argparser.add_argument("--replace_with", default=replace_with)
+
+    # If video_ids file provided, extract list of video IDs from provided file
+    if video_ids:
+        with open(video_ids) as file:
+            video_ids = json.load(file)
+    # Otherwise, default is to find and replace on all videos
+    # Get list of all video ids using an API key
+    else:
+        with open(API_CREDENTIALS_FILE) as file:
+            data = json.load(file)
+            api_key = data["API_KEY"]
+            channel_id = data["CHANNEL_ID"]
+        video_ids = get_video_ids(api_key, channel_id)
 
     # Update each video specified in the video_ids list
     for video_id in video_ids:
@@ -68,8 +88,11 @@ def main(video_ids, find, replace_with):
             update_video(youtube, args)
         except HttpError as e:
             print("An HTTP error {} occurred:\n{}".format(e.resp.status, e.content))
+        except StringNotFoundError as e:
+            info = e.args[0]
+            print(info["message"])
         else:
-            print("Description updated on video with video id '{}'.".format(args.video_id))
+            print("Description updated on video with video ID: '{}'.".format(args.video_id))
 
 
 def get_authenticated_service(args):
@@ -86,6 +109,32 @@ def get_authenticated_service(args):
     return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                  http=credentials.authorize(httplib2.Http()))
 
+
+def get_video_ids(key, channelId):
+    """Get video IDs for all videos on a given channel."""
+    pageToken = ''
+    moreResults = True
+    videoIDs = []
+    while(moreResults):
+        url = 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part={}&maxResults={}&pageToken={}'.format(key, channelId, 'snippet', '50', pageToken)
+        response = requests.get(url)
+        data = response.json()
+        if 'nextPageToken' in data:
+            pageToken = data['nextPageToken']
+        else:
+            moreResults = False
+            pageToken = ''
+        data = data['items']
+        for video in data:
+            if 'videoId' in video['id'] and 'publishedAt' in video['snippet']:
+                videoId = (video['id']['videoId'], video['snippet']['publishedAt'])
+                videoIDs.append(videoId)
+    # sort videoIDs in reverse chronological order based on date published
+    videoIDs.sort(key=lambda x: x[1], reverse=True)
+    # remove publish date from list and return
+    videoIDs = [videoID[0] for videoID in videoIDs]
+    return videoIDs
+
 def update_video(youtube, options):
     # Call the API's videos.list method to retrieve the video resource.
     videos_list_response = youtube.videos().list(
@@ -97,7 +146,6 @@ def update_video(youtube, options):
     # not found.
     if not videos_list_response["items"]:
         print("Video '{}' was not found.".format(options.video_id))
-        # sys.exit(1)
 
     # Since the request specified a video ID, the response only contains one
     # video resource. This code extracts the snippet from that resource.
@@ -107,16 +155,21 @@ def update_video(youtube, options):
         find_string = file.read()
     with open(options.replace_with) as file:
         replace_with_string = file.read()
-    # get rid of newline at end of file
-    find_string = find_string[0:-1]
-    replace_with_string = replace_with_string[0:-1]
+
+    # remove trailing whitespace
+    find_string = find_string.rstrip()
+    replace_with_string = replace_with_string.rstrip()
 
     # Preserve the descriptions already associated with the video. If the video does
     # not have a description, create a new one. Update the description
     if "description" not in videos_list_snippet:
         videos_list_snippet["description"] = ""
     original_description = videos_list_snippet["description"]
-    new_description = original_description.replace(find_string, replace_with_string)
+    new_description = original_description
+    if find_string in original_description:
+        new_description = original_description.replace(find_string, replace_with_string)
+    else:
+        raise StringNotFoundError({"message": "replace string not found in description of video with ID: '{}'".format(options.video_id)})
     videos_list_snippet["description"] = new_description
 
     # Update the video resource by calling the videos.update() method.
